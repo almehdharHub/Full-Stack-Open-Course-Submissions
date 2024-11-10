e6fedf8652a8ded02f7e0574be4c0a1e17adca73
Nawaf almehdhar <181556586+almehdharHub@users.noreply.github.com>
2024-11-11 02:20:16 +0300

    exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,9 +6,13 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault();
-    const newPerson = { name: newName };
-    setPersons(persons.concat(newPerson));
-    setNewName("");
+    if (persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      const newPerson = { name: newName };
+      setPersons(persons.concat(newPerson));
+      setNewName("");
+    }
   };
 
   return (
