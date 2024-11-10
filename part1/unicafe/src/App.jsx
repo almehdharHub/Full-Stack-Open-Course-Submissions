d3a8d0ab00fa3e3c5a3a3fa0e49734afc661b8ff
Nawaf almehdhar <181556586+almehdharHub@users.noreply.github.com>
2024-11-10 04:06:29 +0300

    exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,16 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const all = good + neutral + bad;
+  const average = (good * 1 + neutral * 0 + bad * -1) / all;
+  const positive = (good / all) * 100;
+  console.log("average", average);
+  console.log("good", good);
+  console.log("all", all);
+  console.log("positive", positive);
+  console.log("neutral", neutral);
+  console.log("bad", bad);
+
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -16,6 +26,9 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {all === 0 ? 0 : average}</p>
+      <p>Positive: {all === 0 ? 0 : positive < 0 ? 0 : positive}%</p>
     </div>
   );
 };
